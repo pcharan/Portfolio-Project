d20f6516aa2e34a04af70eebd750790539ae170a
Charan479 <p.charansiddu@gmail.com>
2019-04-24 22:59:09 +0530

    Completd webpage design and it is working fine

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,13 @@ class Blog(models.Model):
 	title = models.CharField(max_length=200);
 	pub_date = models.DateTimeField();
 	body = models.TextField();
-	image = models.ImageField(upload_to='images/');
+	image = models.ImageField(upload_to='images/Blog/');
+
+	def __srt__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
